1ae8f1eccf3387f22ad1f6f9b56743d18aa03899
Tali Tochner <81686032+Taly96@users.noreply.github.com>
2022-07-11 19:44:48 +0300

    Add files via upload

---
+++ GameLogic/GameUpdatedEventArgs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GameLogic
+{
+    public class GameUpdatedEventArgs : EventArgs
+    {
+        private readonly int r_Player1Score = 0;
+        private readonly int r_Player2Score = 0;
+        private readonly uint r_CurrentPlayer = 0;
+
+        public GameUpdatedEventArgs(int i_Player1Score, int i_Player2Score, uint i_CurrentPlayer)
+        {
+            r_CurrentPlayer = i_CurrentPlayer;
+            r_Player1Score = i_Player1Score;
+            r_Player2Score = i_Player2Score;
+        }
+
+        public int Player1Score
+        {
+            get
+            {
+                return r_Player1Score;
+            }
+        }
+
+        public int Player2Score
+        {
+            get
+            {
+                return r_Player2Score;
+            }
+        }
+
+        public uint CurrentPlayer
+        {
+            get
+            {
+                return r_CurrentPlayer;
+            }
+        }
+    }
+}
---
+++ GameLogic/Player.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections.Generic;
+
+namespace GameLogic
+{
+    public class Player
+    {
+        public enum eTypeOfPlayer
+        {
+            Human = 1,
+            Computer,
+        }
+
+        private readonly uint? r_TypeOfPlayer = null;
+        private readonly char? r_SoldierType = null;
+        private readonly char? r_KingType = null;
+        private readonly string r_PlayersFirstName = null;
+        private readonly List<CheckersBoardSquare> r_PlayersSoldiersOnBoard = null;
+        private int m_NumOfKings = 0;
+        private int m_Score = 0;
+
+        public Player(Player i_PlayerToCopy)
+        {
+            r_TypeOfPlayer = i_PlayerToCopy.TypeOfPlayer;
+            r_SoldierType = i_PlayerToCopy.PlayerSoldierType;
+            r_KingType = i_PlayerToCopy.PlayerKingType;
+            r_PlayersFirstName = i_PlayerToCopy.PlayersName;
+            r_PlayersSoldiersOnBoard = new List<CheckersBoardSquare>();
+        }
+
+        public Player(string i_PlayersFirstName, uint i_PlayersType, char i_PlayersSoldierType)
+        {
+            r_TypeOfPlayer = i_PlayersType;
+            r_PlayersFirstName = i_PlayersFirstName;
+            r_SoldierType = i_PlayersSoldierType;
+            r_PlayersSoldiersOnBoard = new List<CheckersBoardSquare>();
+            r_KingType = i_PlayersSoldierType == (char)CheckersBoardSquare.eSquareType.SoldierX
+                             ? (char)CheckersBoardSquare.eSquareType.KingK
+                             : (char)CheckersBoardSquare.eSquareType.KingU;
+        }
+
+        public int NumberOfKingsOnBoard
+        {
+            get
+            {
+                return m_NumOfKings;
+            }
+
+            set
+            {
+                m_NumOfKings = value;
+            }
+        }
+
+        public List<CheckersBoardSquare> PlayersSoldiersOnBoard
+        {
+            get
+            {
+                return r_PlayersSoldiersOnBoard;
+            }
+        }
+
+        public int Score
+        {
+            get
+            {
+                return m_Score;
+            }
+
+            set
+            {
+                m_Score = value;
+            }
+        }
+
+        public char PlayerSoldierType
+        {
+            get
+            {
+                return r_SoldierType.Value;
+            }
+        }
+
+        public string PlayersName
+        {
+            get
+            {
+                return r_PlayersFirstName;
+            }
+        }
+
+        public int NumberOKingsAndSoldiersOnBoard
+        {
+            get
+            {
+                return r_PlayersSoldiersOnBoard.Count;
+            }
+        }
+
+        public int NumberOfSoldiersOnBoard
+        {
+            get
+            {
+                return r_PlayersSoldiersOnBoard.Count - m_NumOfKings;
+            }
+        }
+
+        public uint TypeOfPlayer
+        {
+            get
+            {
+                return r_TypeOfPlayer.Value;
+            }
+        }
+
+        public char PlayerKingType
+        {
+            get
+            {
+                return r_KingType.Value;
+            }
+        }
+
+        public void Reset()
+        {
+            m_NumOfKings = 0;
+
+            r_PlayersSoldiersOnBoard.Clear();
+        }
+
+        public void RemoveSoldier(CheckersBoardSquare i_NoLongerPlayersSquare)
+        {
+            r_PlayersSoldiersOnBoard.Remove(i_NoLongerPlayersSquare);
+        }
+
+        public void AddSoldier(CheckersBoardSquare i_NewSquareForPlayer)
+        {
+            r_PlayersSoldiersOnBoard.Add(i_NewSquareForPlayer);
+        }
+    }
+}
